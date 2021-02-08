9f5e24f7e7e5dea9f33f1bfa1f532b0a8a63931b
dill337 <64544519+dill337@users.noreply.github.com>
2021-02-08 15:35:09 -0600

    made back button

--- index.js
+++ index.js
@@ -36,16 +36,16 @@ const cors = require('cors');
 
 
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORs policy for this application doesn\'t allow acces from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       let message = 'The CORs policy for this application doesn\'t allow acces from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 let allowedOrigins = ['*']
 // ['http://localhost:8080',
@@ -54,7 +54,7 @@ let allowedOrigins = ['*']
 //   'https://myfilmm.herokuapp.com/movies'
 // ]
 
-// app.use(cors());
+app.use(cors());
 
 
 // Get requests
